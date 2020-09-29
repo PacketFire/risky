b2f2a6dcfe15bfe8676bc221a54db66639604438
Raas Ahsan <raas.ahsan@gmail.com>
2020-09-28 22:07:12 -0500

    Remove binary caching for now

--- www/lib/App.svelte
+++ www/lib/App.svelte
@@ -1,16 +1,7 @@
 <script>
   import * as wasm from 'crank';
 
-  // // wasm.greet('visitor');
-
-  // document.getElementById('text').addEventListener('input', function (event) {
-  //   // var result = wasm.parse_formula(event.target.value);
-  //   // if (result) {
-  //   //   document.getElementById('result').innerHTML = 'VALID';
-  //   // } else {
-  //   //   document.getElementById('result').innerHTML = 'INVALID';
-  //   // }
-  // })
+  // wasm.greet('visitor');
 </script>
 
 <style>
